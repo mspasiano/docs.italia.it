ae8a217ea443eb7b1959d2ad0c79083d78272bec
Carmelo Catalfamo <carmelo.catalfamo92@gmail.com>
2019-10-17 13:48:37 +0200

    Tags and media queries

--- media/javascript/search-full-screen.js
+++ media/javascript/search-full-screen.js
@@ -3,9 +3,34 @@
  * Reference to: https://github.com/italia/docs.italia.it/issues/314
  */
 
+var debounceInputTiming = 300
+var params = {
+  filter: 'all',
+  search: ''
+}
+
+var pendingRequest = null
+var fetchResultsFromApi = function () {
+  // TODO: Fetch here results from API
+  if (pendingRequest) pendingRequest.abort('Canceled from user')
+
+  console.log('Fetch API with those params', params)
+
+  pendingRequest = $.ajax('https://jsonplaceholder.typicode.com/users')
+    .done(function (response) {
+      console.log('response', response)
+    })
+    .fail(function (error) {
+      if (error.statusText !== 'Canceled from user') {
+        console.log('fetchResultsFromApi error:', error)
+      }
+    })
+}
+
 $(document).ready(function () {
   var modal = $('#modalSearchFullScreen')
   var input = $('#autocompleteSearchFullScreen')
+  var tagButtons = $('.sfs-btn-tag')
 
   // Options
   // https://getbootstrap.com/docs/4.0/components/modal/#options
@@ -34,8 +59,50 @@ $(document).ready(function () {
 
   })
 
-  input.on('change paste keyup', function () {
-    var value = $(this).val()
-    console.log(value)
- });
+  input.on('paste keyup', debounce(function () {
+    params.search = $(this).val()
+    fetchResultsFromApi()
+  }, debounceInputTiming))
+
+  tagButtons.on('click', function () {
+    var filter = $(this).attr('data-filter')
+    params.filter = filter
+
+    tagButtons.each(function () {
+      $(this).removeClass('btn-primary btn-outline-secondary')
+      $(this).children('.icon').removeClass('icon-white icon-secondary')
+
+      var isActive = $(this).attr('data-filter') === filter
+      $(this).addClass(isActive ? 'btn-primary' : 'btn-outline-secondary')
+      $(this).children('.icon').addClass(isActive ? 'icon-white' : 'icon-secondary')
+    })
+
+    fetchResultsFromApi()
+  })
 })
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+var debounce = function (func, wait, immediate) {
+  var timeout
+
+  return function () {
+    var context = this
+    var args = arguments
+
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+
+    if (callNow) {
+      func.apply(context, args)
+    }
+  }
+}
