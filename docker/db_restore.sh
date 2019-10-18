b7f61e9c78554ab127f240d28824b79708744daa
Paolo Melchiorre <paolo@20tab.com>
2019-10-18 16:25:41 +0200

    Fix db_restore script

--- docker/db_restore.sh
+++ docker/db_restore.sh
@@ -14,10 +14,8 @@ fi
 echo "Restoring database from '$1'"
 docker-compose up -d "${DB_CONTAINER}"
 sleep 10
-docker-compose exec -T "${DB_CONTAINER}" dropdb -U postgres "${DB_NAME}" --if-exists
-docker-compose exec -T "${DB_CONTAINER}" dropuser -U postgres "${DB_USER}" --if-exists
-docker-compose exec -T "${DB_CONTAINER}" createuser -U postgres "${DB_USER}"
-docker-compose exec -T "${DB_CONTAINER}" createdb -U postgres -O "${DB_USER}" "${DB_NAME}"
+docker-compose exec -T "${DB_CONTAINER}" dropdb -U "${DB_USER}" "${DB_NAME}" --if-exists
+docker-compose exec -T "${DB_CONTAINER}" createdb -U "${DB_USER}" -O "${DB_USER}" "${DB_NAME}"
 ext=${1##*\.}
 case "$ext" in
 sql) docker-compose exec -T "${DB_CONTAINER}" psql -U "${DB_USER}" -d "${DB_NAME}" < "${1}"
