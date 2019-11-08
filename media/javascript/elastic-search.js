8263bc27571fa5b50b165df8f4ec124ef523f1aa
Carmelo Catalfamo <carmelo.catalfamo92@gmail.com>
2019-11-08 11:48:36 +0100

    Fix order results when user enter on page

--- media/javascript/elastic-search.js
+++ media/javascript/elastic-search.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+  var params = Qs.parse(window.location.search.replace('?', ''))
+
+  var orderValues = ['date', '-date', 'name']
+
+  if (params.sort && orderValues.includes(params.sort)) {
+    $('#order-select').val(params.sort)
+  }
+
   $('#order-select').change(function () {
     var selectedOrder = $(this).children('option:selected').val()
     var params = Qs.parse(window.location.search.replace('?', ''))
