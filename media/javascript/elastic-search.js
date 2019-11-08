e2306cdb2b6f3504b68c6c63f4a09e99cc32bd42
Carmelo Catalfamo <carmelo.catalfamo92@gmail.com>
2019-11-08 13:13:52 +0100

    options with for loop

--- media/javascript/elastic-search.js
+++ media/javascript/elastic-search.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var selectedOrder = $(this).children('option:selected').val()
     var params = Qs.parse(window.location.search.replace('?', ''))
 
-    if (selectedOrder) {
+    if (selectedOrder && selectedOrder !== 'relevance') {
       params.sort = selectedOrder
     } else {
       if (params.sort) delete params.sort
