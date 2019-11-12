b2821e2d7a1e4cb847a82fb089e0ba6da046f7dd
Carmelo Catalfamo <carmelo.catalfamo92@gmail.com>
2019-11-12 16:22:10 +0100

    Fixes on js and styles

--- media/javascript/elastic-search.js
+++ media/javascript/elastic-search.js
@@ -1,12 +1,14 @@
+// Qs is defined on readthedocs/templates/docsitalia/overrides/search/elastic_search.html on bottom
+
 $(document).ready(function () {
-  $('#order-select').change(function () {
+  $('#order-select').on('change', function () {
     var selectedOrder = $(this).children('option:selected').val()
     var params = Qs.parse(window.location.search.replace('?', ''))
 
     if (selectedOrder && selectedOrder !== 'relevance') {
       params.sort = selectedOrder
     } else {
-      if (params.sort) delete params.sort
+      delete params.sort
     }
 
     window.location.href = window.location.origin + window.location.pathname + '?' + Qs.stringify(params)
