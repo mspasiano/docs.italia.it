e3d0a995f48019f22f52df621253410c536a5191
Iacopo Spalletti <i.spalletti@nephila.it>
2019-11-19 18:33:02 +0100

    Add command to competely rebuild indices and reindexing ES

---
+++ docker/es_rebuild_indices.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -e
+
+echo "\n========\nRecreating ES indices and reindexing the content\n========\n"
+docker-compose up -d web celery-web
+sleep 5
+docker-compose exec -T web python manage.py search_index --rebuild
+docker-compose exec -T web python manage.py reindex_elasticsearch
