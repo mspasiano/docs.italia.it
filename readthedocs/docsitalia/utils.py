7ec1a213c83ffdd443df7a179afe23d95955e77b
Serge Fedoruk <s.fedoruk@nephila.digital>
2019-12-10 10:37:07 +0100

    #247 Change version slug in urls for not-italian documents (#375)
    * Change version slug in urls for not-italian documents
    * Use itresolver instead of changing Project.get_docs_url

--- readthedocs/docsitalia/utils.py
+++ readthedocs/docsitalia/utils.py
@@ -9,9 +9,14 @@ import yaml
 # pylint: disable=redefined-builtin
 from requests.exceptions import ConnectionError
 
+from django.conf import settings
+
+from readthedocs.builds.constants import LATEST, STABLE
 from readthedocs.builds.models import Build
 from readthedocs.projects.models import Project
 from readthedocs.api.v2.client import api as apiv2
+
+from . import LANG_IT
 
 
 def load_yaml(txt):
@@ -66,3 +71,38 @@ def get_projects_with_builds(only_public=True):
     return Project.objects.filter(
         pk__in=filtered_projects
     )
+
+
+def get_international_version_slug(project, lang_slug, version_slug):
+    """For readability of urls we are changing slug names if language is not Italian."""
+    try:
+        lang_slug = lang_slug or project.language
+        version_slug = version_slug or project.get_default_version()
+        if lang_slug != LANG_IT:
+            if version_slug == STABLE:
+                return settings.RTD_STABLE_EN
+            if version_slug == LATEST:
+                return settings.RTD_LATEST_EN
+    except AttributeError:
+        # this means custom versions are not set and we fallback to unchanged behavior
+        pass
+    return version_slug
+
+
+def get_real_version_slug(lang_slug, version_slug):
+    """
+    If language is not Italian - get real slug name from international one.
+
+    International versions (RTD_STABLE_EN/RTD_LATEST_EN) are provided only for readability purpose,
+    only STABLE/LATEST are stored in DB.
+    """
+    try:
+        if lang_slug != LANG_IT:
+            if version_slug == settings.RTD_STABLE_EN:
+                version_slug = STABLE
+            if version_slug == settings.RTD_LATEST_EN:
+                version_slug = LATEST
+    except AttributeError:
+        # this means custom versions are not set and we fallback to unchanged behavior
+        pass
+    return version_slug
