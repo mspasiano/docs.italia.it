13fe75a49ce7e983cc95587a8c6f9a2fc1a41d80
Iacopo Spalletti <i.spalletti@nephila.it>
2019-12-24 14:00:03 +0100

    Ripristina pagine di errore custom perse durante l'upgrade

--- readthedocs/urls.py
+++ readthedocs/urls.py
@@ -21,8 +21,12 @@ from readthedocs.search import views as search_views
 from readthedocs.search.api import PageSearchAPIView
 
 
+from readthedocs.docsitalia.views.core_views import server_error_401
+
+
 admin.autodiscover()
 
+handler401 = server_error_401
 handler404 = server_error_404
 handler500 = server_error_500
 
